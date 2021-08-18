edd3dae71e294f3680fae78373c9ae8177022f49
DragoRyu <51171600+Sanchith26@users.noreply.github.com>
2021-08-18 08:46:08 +0530

    Node 1

---
+++ Node1.py
@@ -0,0 +1,11 @@
+inputs=[2.0,4.0,5.0,6.0]
+weights=[6.5, 4.5, -0.5, 2.0]
+bias = 2
+output = 0
+for input, weight in zip(inputs, weights):
+    output = output + input*weight
+    print(str(input) + " * " + str(weight) + " + ")
+print(bias)
+output += bias
+
+print(output)
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Learning-Deep-Learning
 This Repository is made for learning Deep learning
+
+1) Logic for One Node
